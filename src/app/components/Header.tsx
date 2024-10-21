3242a68cdda8da6c75d4a61829b2b0f5ad19d238
Umar Farooqi <umar_farooqi@hotmail.co.uk>
2024-10-21 01:20:52 +0100

    start some responsive design

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,8 +6,12 @@ import {
     navigationMenuTriggerStyle
 } from '@/components/ui/navigation-menu';
 import { ReactNode } from 'react';
+import Image from 'next/image';
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { Button } from '@/components/ui/button';
+import { HamburgerMenuIcon } from '@radix-ui/react-icons';
 
-const NextNavigationMenuLink = ({href, children}: { href: string, children: ReactNode }) => (
+const NextNavigationMenuLink = ({ href, children }: { href: string, children: ReactNode }) => (
     <Link href={href} passHref legacyBehavior>
         <NavigationMenuLink className={navigationMenuTriggerStyle()}>
             {children}
@@ -15,25 +19,48 @@ const NextNavigationMenuLink = ({href, children}: { href: string, children: Reac
     </Link>
 );
 
+const AdoptionContent = () => (
+    <div>
+        <NextNavigationMenuLink href="/adoption/kittens">
+            <Image src="/kitten.webp" width={50} height={50} alt="Kitten" />
+            <div className="text-lg">Cats</div>
+        </NextNavigationMenuLink>
+        <NextNavigationMenuLink href="/adoption/kittens">
+            <Image src="/kitten.webp" width={50} height={50} alt="Kitten" />
+            <div className="text-lg">Kittens</div>
+        </NextNavigationMenuLink>
+    </div>
+);
+
 const Header = () => (
-    <div className="flex justify-between">
-        <Link href="/">Logo</Link>
-        <div className="flex gap-2">
+    <header className="w-1/2 flex justify-between">
+        {/* TODO: convert this sheeit to sidebar */}
+        <Sheet>
+            <SheetTrigger asChild>
+                <Button className="lg:hidden" variant="outline" size="icon">
+                    <HamburgerMenuIcon />
+                </Button>
+            </SheetTrigger>
+            <SheetContent side="left">
+                <div className="flex flex-col gap-2 py-5">
+                    <Link href="/public" className="text-lg">Home</Link>
+                    <Link href="/portfolio" className="text-lg">Portfolio</Link>
+                    <Link href="/blog" className="text-lg">Blog</Link>
+                    <Link href="/contact-me" className="text-lg">Contact Me</Link>
+                </div>
+            </SheetContent>
+        </Sheet>
+        <Link href="/" className="lg:">Logo</Link>
+        <nav className="hidden lg:flex gap-2">
             <NavigationMenu>
                 <NavigationMenuList>
                     {/* TODO: seeing if it still works if you don't wrap each thing in NavigationMenuItem unless it's
                         got content. Put it back in if stuff starts going wrong with the header links */}
                     <NextNavigationMenuLink href="/">Home</NextNavigationMenuLink>
                     <NavigationMenuItem>
-                        {/* TODO: funky stuff going on here because the link inside the trigger has it's own on:hover separate to the trigger - fix it */}
-                        <NavigationMenuTrigger>
-                            <NextNavigationMenuLink href="/adoption">Adoption</NextNavigationMenuLink>
-                        </NavigationMenuTrigger>
+                        <NavigationMenuTrigger>Adoption</NavigationMenuTrigger>
                         <NavigationMenuContent>
-                            <ul>
-                                <li><NextNavigationMenuLink href={"/adoption/cats"}>Cats</NextNavigationMenuLink></li>
-                                <li><NextNavigationMenuLink href={"/adoption/kittens"}>Kittens</NextNavigationMenuLink></li>
-                            </ul>
+                            <AdoptionContent />
                         </NavigationMenuContent>
                     </NavigationMenuItem>
                     <NextNavigationMenuLink href="/fostering">Fostering</NextNavigationMenuLink>
@@ -41,8 +68,8 @@ const Header = () => (
                     <NextNavigationMenuLink href="/contact-us">Contact Us</NextNavigationMenuLink>
                 </NavigationMenuList>
             </NavigationMenu>
-        </div>
-    </div>
+        </nav>
+    </header>
 );
 
 export default Header;
