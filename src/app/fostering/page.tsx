db6a3bbef602e912234cb516ff8c5babb6d0dccd
Umar Farooqi <umar_farooqi@hotmail.co.uk>
2025-07-06 17:03:58 +0100

    Add Fostering page content. Homepage banner and other tweaks

--- src/app/fostering/page.tsx
+++ src/app/fostering/page.tsx
@@ -1,10 +1,43 @@
 import { PageHeader } from '@/components/PageHeader';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 const Fostering = () => (
     <>
         <PageHeader>
             <h1>Fostering</h1>
         </PageHeader>
+        <section className="px-6 py-8">
+            <h2>Fostering for MCR</h2>
+            <div>
+                <p>
+                    Here at MCR, we are always looking for new foster carers for our cats. We have cats and kittens of
+                    all ages, temperaments, and health statuses so we normally find someone to suit your situation.
+                </p>
+                <br />
+                <p>
+                    Whether you would prefer kittens or nursing mums, battered lost boys, or sanctuary cats looking for
+                    somewhere to live out the rest the rest of their lives, we can find the cat for you.
+                </p>
+                <br />
+                <p>
+                    Cats in foster care are covered for all veterinary costs and care under one of our registered vet
+                    practices. We also have a friendly foster group on Facebook where you can ask for advice or just
+                    share fun photos and stories with your fellow fosterers.
+                </p>
+                <br />
+                <p>If fostering sounds like something you would enjoy, please complete the form linked below.</p>
+            </div>
+            <div className="mt-5 flex max-lg:justify-center">
+                {/* TODO: see if there's a short link for this form that doesn't go through Facebook*/}
+                <Button
+                    asChild
+                    className="h-10 w-full max-w-72 bg-secondary text-secondary-foreground shadow-2xl hover:bg-accent hover:text-accent-foreground"
+                >
+                    <Link href="https://airtable.com/appxlbpsyoahJocVA/shrQnwZyQlO04xlKF">Apply to foster</Link>
+                </Button>
+            </div>
+        </section>
     </>
 );
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,90 @@
-
 // TODO: sliding banner of cats
 import Logo from '@/components/Logo';
+import Link from 'next/link';
 
 const Home = () => (
-    <div className="flex">
-        <Logo />
-        <h1 className="font-serif text-2xl">Mansfield Cat Rescue Logo</h1>
-        <p>Mansfield Cat Rescue is a volunteer only, UK-registered charity operating Mansfield, Nottinghamshire</p>
-        <h2>Our mission is to rescue, the dream is that one day we don’t have to</h2>
-        <p>MCR rescues, rehabilitates, and re-homes stray, abandoned, and feral cats blah blah get this from Faeboo</p>
-    </div>
+    <main>
+        <section className="flex flex-col items-center bg-gradient-to-b from-teal-400 to-green-400 px-6 py-36 text-center">
+            {/* TODO: fade-in animation on logo? */}
+            {/* TODO: other loading animations? loading spinner */}
+            <div className="mb-9">
+                {/*<Logo />*/}
+                <img src="/Black%20Logo.png" alt="Mansfield Cat Rescue Logo" />
+            </div>
+            <p className="text-xs">
+                Mansfield Cat Rescue is a volunteer only, UK-registered charity (charity number: 1193319) operating in
+                Mansfield, Nottinghamshire
+            </p>
+        </section>
+        <div className="flex justify-center">
+            <img src="/banner.webp" alt="Our mission is to rescue, the dream is that one day we don't have to" />
+        </div>
+        <section className="px-6 py-8">
+            <h2 className="text-center">About Us</h2>
+            <div className="text-sm">
+                <p>
+                    Mansfield Cat Rescue rescues, rehabilitates and rehomes stray, abandoned, and feral cats. All of our
+                    cats and kittens are fostered and looked after in a home environment by our volunteers.
+                </p>
+                <p>
+                    We have accounts with a number of vet practices that provide veterinary care to our cats and kittens
+                    including; spay/neuter, flea and worm medicines, and treatment for abscesses and other large wounds
+                    as needed. Our vets also carry out more complicated procedures such as dental work, and damaged eye
+                    or ear removal as recommended. Any cats or kittens that are not suitable for adoption due to ongoing
+                    health issues, will be looked after by special long-term foster carers.
+                </p>
+                <p>
+                    All those involved in Mansfield Cat Rescue’s mission do so on a volunteer basis with no
+                    renumeration. Our vet bills are paid for by donations and fundraising activities.{' '}
+                </p>
+                <p>
+                    If you would like to enquire about adopting a cat or kitten from Mansfield Cat Rescue, please{' '}
+                    <Link href="https://bit.ly/3ucplsL" target="_blank" rel="noopener noreferrer">
+                        click here
+                    </Link>
+                </p>
+                <p>
+                    If you would like to view the cats and kittens that Mansfield Cat Rescue have available to adopt,
+                    see our <Link href="/adoption">Adoption page</Link>
+                </p>
+                <p>
+                    If you would like to donate to Mansfield Cat Rescue, you can do so via any of the methods provided
+                    below:
+                </p>
+                <ul className="list-none">
+                    <li>
+                        <p className="font-semibold">Bank Transfer</p>
+                        <p>
+                            Nat West
+                            <br />
+                            Mansfield Cat Rescue
+                            <br />
+                            Sort code: 54-10-23
+                            <br />
+                            Account number: 19376804
+                        </p>
+                    </li>
+                    <li>
+                        <p className="font-semibold">Paypal</p>
+                        <p>
+                            admin@mansfieldcatrescue.co.uk - (please select family and friends when making the donation
+                            so that PayPal don't take a % of your donation before it reaches us)
+                        </p>
+                    </li>
+                </ul>
+                <p>
+                    If you would like to purchase items for the cats and kittens in our care, please see our{' '}
+                    <Link
+                        href="https://www.amazon.co.uk/hz/wishlist/ls/1UDC50S3D85VY?ref_=wl_share"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Amazon Wishlist
+                    </Link>
+                </p>
+            </div>
+        </section>
+    </main>
 );
 
 export default Home;
